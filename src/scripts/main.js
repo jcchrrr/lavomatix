1b9713ef77c3623a87f18181b28cbda29c494a40
Jean-Charles Charrier <jean-charles.charrier@havasdigitalfactory.com>
2019-03-01 14:54:15 +0100

    remove branding

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,9 +4,6 @@ import SampleComponent from "./components/SampleComponent"
 
 const main = {
     init: function() {
-        console.log("%cHavas Digital Factory", 'background: #ff1e2f; color: white; padding:10px;');
-        console.log("%chttps://havasdigitalfactory.com", 'background: black; color: #333; padding:10px;');
-
         SampleComponent.init();
     }
 }
